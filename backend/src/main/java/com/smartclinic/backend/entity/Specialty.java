ea6269bd603709e910a6f066866f4fd1e6ef8934
Nguyen Ngoc Quang <quangnnhe182062@fpt.edu.vn>
2026-07-23 12:31:12 +0700

    chore: save local modifications

--- backend/src/main/java/com/smartclinic/backend/entity/Specialty.java
+++ backend/src/main/java/com/smartclinic/backend/entity/Specialty.java
@@ -17,10 +17,8 @@ public class Specialty {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    @org.hibernate.annotations.Nationalized
     private String name;
 
     @Column(length = 1000)
-    @org.hibernate.annotations.Nationalized
     private String description;
 }
